ffb71daca516f10877e55a8e43ff115695bb5968
Mudassir Ali <kmudassirali@outlook.com>
2025-02-03 10:25:12 -0500

    Update Init.sh

--- Init.sh
+++ Init.sh
@@ -6,13 +6,10 @@ sudo apt upgrade
 
 sudo apt install openjdk-17-jdk -y
 
-echo java -version
-
-openjdk version "17.0.13" 2024-10-15
-
-OpenJDK Runtime Environment (build 17.0.13+11-Debian-2)
-
-OpenJDK 64-Bit Server VM (build 17.0.13+11-Debian-2, mixed mode, sharing)
+java -version
+# openjdk version "17.0.13" 2024-10-15
+# OpenJDK Runtime Environment (build 17.0.13+11-Debian-2)
+# OpenJDK 64-Bit Server VM (build 17.0.13+11-Debian-2, mixed mode, sharing)
 
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian/jenkins.io-2023.key
@@ -23,4 +20,4 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
 
 sudo apt-get update
 
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
